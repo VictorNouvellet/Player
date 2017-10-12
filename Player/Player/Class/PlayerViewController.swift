60bf3c884f038ac16f6936826e2c22908c60e2c8
Victor Nouvellet <victor.nouvellet@gmail.com>
2017-10-12 22:13:34 +0200

    Add Cocoapod system, add MarqueeLabel & MBProgressHUD pods, add Player's buttons images
    - Add Cocoapod system
    - Add MarqueeLabel & MBProgressHUD pods
    - Add PlayerViewController (PVC)
    - Add PVC's buttons images

---
+++ Player/Player/Class/PlayerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PlayerViewController.swift
+//  Player
+//
+//  Created by Victor Nouvellet on 10/12/17.
+//  Copyright © 2017 Victor Nouvellet. All rights reserved.
+//
+
+import Foundation
