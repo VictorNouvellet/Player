b2b1f37213bab3b359b18eb754bd0e39b9b4ddf7
Victor Nouvellet <victor.nouvellet@gmail.com>
2017-10-15 03:08:25 +0200

    Refactor PlayerViewController, make it listen to PlayerManager notifications

--- Player/Player/Class/PlayerViewController.swift
+++ Player/Player/Class/PlayerViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import AVKit
 import MarqueeLabel
 
 class PlayerViewController: UIViewController {
@@ -20,28 +19,34 @@ class PlayerViewController: UIViewController {
     @IBOutlet var playButton: UIButton!
     @IBOutlet var pauseButton: UIButton!
     
-    var player = AVPlayer()
-    var song : SongModel? = nil
+    var song : SongModel? {
+        return PlayerManager.shared.song
+    }
     
     // MARK: Parent methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Setup play/pause notification
+        setupNotification()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
         // Configure view
         guard let safeSong = song, let safePreviewUrl = URL(string:safeSong.iTunesPreviewUrl) else {
             print("No song loaded to player because the iTunes Preview URL is wrong")
             return
         }
-        self.player = AVPlayer(url: safePreviewUrl)
-        self.nameLabel.text = safeSong.name
-        self.artistLabel.text = safeSong.artistName
-        self.backgroundImageView.setImageFromURL(url: safeSong.artworkUrl)
-        self.artworkImageView.setImageFromURL(url: safeSong.artworkUrl)
+        
+        self.setupSongInformation(song: safeSong, songUrl: safePreviewUrl)
+        self.updatePlayPauseState()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        self.play()
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
     // MARK: Interface Builder methods
@@ -57,16 +62,51 @@ class PlayerViewController: UIViewController {
         self.pause()
     }
     
+    @IBAction func previousButtonTouched(_ sender: Any) {
+        PlayerManager.shared.previous()
+    }
+    
+    @IBAction func nextButtonTouched(_ sender: Any) {
+        PlayerManager.shared.next()
+    }
+    
+    // MARK: public methods
+    
+    @objc func onPlayerStateUpdateNotification(_ notification: Notification) {
+        self.updatePlayPauseState()
+    }
+    
     // MARK: private methods
     private func play() {
-        self.playButton.isHidden = true
-        self.pauseButton.isHidden = false
-        player.play()
+        PlayerManager.shared.play()
     }
     
     private func pause() {
-        self.playButton.isHidden = false
-        self.pauseButton.isHidden = true
-        player.pause()
+        PlayerManager.shared.pause()
     }
+    
+    private func updatePlayPauseState() {
+        if let player = PlayerManager.shared.player, player.isPlaying {
+            self.playButton.isHidden = true
+            self.pauseButton.isHidden = false
+        } else {
+            self.playButton.isHidden = false
+            self.pauseButton.isHidden = true
+        }
+    }
+    
+    private func setupSongInformation(song: SongModel, songUrl: URL!) {
+        self.nameLabel.text = song.name
+        self.artistLabel.text = song.artistName
+        self.backgroundImageView.setImageFromURL(url: song.artworkUrl)
+        self.artworkImageView.setImageFromURL(url: song.artworkUrl)
+    }
+    
+    private func setupNotification() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(onPlayerStateUpdateNotification(_:)),
+                                               name: PlayerManager.playerStateChangedNotificationName,
+                                               object: nil)
+    }
+    
 }
